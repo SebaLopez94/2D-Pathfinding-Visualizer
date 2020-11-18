99bb67505e6310834336843127ee4e09b4e15034
SebaLG <sebalg180@gmail.com>
2020-11-18 20:32:02 +0100

    Color Saturation

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 export default class Constants {
-  static ROWS = 20;
+  static ROWS = 18;
   static COLUMNS = 40;
 
-  static START_NODE_ROW = 10;
-  static START_NODE_COL = 15;
-  static FINISH_NODE_ROW = 10;
-  static FINISH_NODE_COL = 35;
+  static START_NODE_ROW = 8;
+  static START_NODE_COL = 10;
+  static FINISH_NODE_ROW = 8;
+  static FINISH_NODE_COL = 30;
 }
