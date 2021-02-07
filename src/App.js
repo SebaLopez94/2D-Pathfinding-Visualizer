d28562f3dd62dcd1490e34f6673ce561f634a521
SebaLG <sebalg180@gmail.com>
2021-02-07 02:11:04 +0100

    Mobile not available

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./App.css";
 import Grid from "./components/Grid.jsx";
+import {isMobile} from 'react-device-detect';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <Grid></Grid>
-    </div>
-  );
+  if (!isMobile) {
+    return (
+      <div className="App">
+        <Grid></Grid>
+      </div>
+    );
+  }else{
+    return (
+      <div className="mobile-not-available">
+        <h3>This content is unavailable on mobile</h3>
+      </div>
+    );
+  }
+
 }
 
 export default App;
